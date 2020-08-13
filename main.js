6c4d152a4a995ca549f7e2a6e7a2efdf99e0c56a
sergey <mketsvil2@gmail.com>
2020-08-14 02:26:35 +0600

    gameBot

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
-'use strict';
+"use strict";
 
 let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
+	return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
 let createGame = function () {
-    let correctNumber = Math.floor(Math.random() * 101);
-    console.log('Загаданное число', correctNumber);
+	let correctNumber = Math.floor(Math.random() * 101);
+	console.log(correctNumber);
 
-    let getNumber = function () {
+	let getNumber = function () {
+		let getWish = function (flag) {
+			if (flag) {
+				getNumber();
+			} else {
+				alert("Спасибо, что поиграли со мной");
+			}
+		};
 
-        let getWish = function (flag) {
-            if (flag) {
-                getNumber();
-            } else {
-                alert('Спасибо, что поиграли со мной');
-            }
-        };
-
-        let wish,
-            userNumber = prompt('Угадай число от 1 до 100');
-        if (userNumber === null) {
-            alert('Может, в следующий раз...');
-        } else if (!isNumber(userNumber) || (parseFloat(userNumber) > 100)) {
-            wish = confirm('Введи число от 0 до 100!');
-            getWish(wish);
-        } else if (userNumber == correctNumber) {
-            alert('Ты угадал!');
-        } else if (userNumber > correctNumber) {
-            wish = confirm('Загаданное число меньше. Попробуй еще');
-            getWish(wish);
-        } else if (userNumber < correctNumber) {
-            wish = confirm('Загаданное число больше. Попробуй еще');
-            getWish(wish);
-        }
-    };
-    return getNumber;
+		let wish,
+			userNumber = prompt("Угадай число от 1 до 100");
+		if (userNumber === null) {
+			alert("Может, в следующий раз...");
+		} else if (!isNumber(userNumber) || parseFloat(userNumber) > 100) {
+			wish = confirm("Введи число от 0 до 100!");
+			getWish(wish);
+		} else if (userNumber == correctNumber) {
+			alert("Ты угадал!");
+		} else if (userNumber > correctNumber) {
+			wish = confirm("Загаданное число меньше. Попробуй еще");
+			getWish(wish);
+		} else if (userNumber < correctNumber) {
+			wish = confirm("Загаданное число больше. Попробуй еще");
+			getWish(wish);
+		}
+	};
+	return getNumber;
 };
 
 let game = createGame();
